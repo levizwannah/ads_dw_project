88a3407ff0b79086826b0cb296d979c519a0001b
levizwannah <levizwannah@gmail.com>
2021-08-19 14:46:13 +0300

    working on the sql for the cubes

--- warehouse.sql
+++ warehouse.sql
@@ -34,16 +34,39 @@ create table `dimLocation`(
     `long` float ,
     `location_name` varchar(1000) not null,
     `mpi` float not null,
-     FOREIGN KEY (`country_id`) REFERENCES `country`(`country_id`),
-     FOREIGN KEY (`region_id`) REFERENCES `country`(`country_id`),
-     FOREIGN KEY (`w_region_id`) REFERENCES `country`(`country_id`)
+     FOREIGN KEY (`country_id`) REFERENCES `dimCountry`(`country_id`),
+     FOREIGN KEY (`region_id`) REFERENCES `dimRegion`(`region_id`),
+     FOREIGN KEY (`w_region_id`) REFERENCES `dimWorldRegion`(`w_region_id`)
 );
 
--- Loan theme --
+-- Loan theme type --
 create table `dimLoanThemeType`(
-    `loan_theme_id` bigint unsigned not null primary key AUTO_INCREMENT,
+    `theme_type_id` bigint unsigned not null primary key AUTO_INCREMENT,
     `loan_theme_type` varchar(255) not null
 );
+
+-- Partner dimenstion --
+create table `dimPartner`(
+    `t_partner_id` BIGINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `partner_id` int unsigned,
+    `partner_name` varchar(300),
+    `theme_type_id` BIGINT UNSIGNED NOT NULL,
+    
+     FOREIGN KEY (`theme_type_id`) REFERENCES `dimLoanThemeType`(`theme_type_id`) 
+)
+
+-- loan theme grant
+create table `dimLoanThemeGrant`(
+    `loan_theme_grant_id` bigint unsigned not null primary key AUTO_INCREMENT,
+    `location_id` bigint unsigned not null,
+    `t_partner_id` bigint unsigned not null,
+    `loan_theme_number` int,
+    `loan_theme_amount` float not null,
+    FOREIGN KEY (`location_id`) REFERENCES `dimLocation`(`location_id`),
+    FOREIGN KEY (`t_patner_id`) REFERENCES `dimPartner`(`t_partner_id`),
+);
+
+--
 
 
 
